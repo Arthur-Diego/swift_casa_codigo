2dec8e20c585ac91833552bc97914eff226db919
Arthur Diego <arthurdiego@Arthurs-Mac.local>
2017-01-13 14:53:38 -0300

    Código swift casa do código cap 10

---
+++ eggplant-brownie/Item.swift
@@ -0,0 +1,24 @@
+//
+//  Item.swift
+//  eggplant-brownie
+//
+//  Created by Arthur Diego on 10/01/17.
+//  Copyright © 2017 br.com.alura. All rights reserved.
+//
+
+import Foundation
+
+class Item: Equatable{
+    
+    let name: String;
+    let calories: Double;
+    
+    init(name: String, calories: Double){
+        self.name = name;
+        self.calories = calories;
+    }
+}
+func ==(first:Item, second:Item) -> Bool {
+    
+    return first.name == second.name && first.calories == second.calories
+}
